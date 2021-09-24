6553638707ae841b7ca48c96b50c39a834927411
alejandro toledo <atoledofr@gmail.com>
2021-09-23 21:33:01 -0500

    udpate routes for the search and form_validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :searches
+  namespace :form_validations do
+    resources :searches, only: [:create, :update]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
