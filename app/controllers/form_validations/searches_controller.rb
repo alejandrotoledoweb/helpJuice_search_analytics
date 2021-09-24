e839307ff19a9ce9f8e3fac79aa9789f44212619
alejandro toledo <atoledofr@gmail.com>
2021-09-24 11:01:41 -0500

    fix render partial in form valitons controller js file

--- app/controllers/form_validations/searches_controller.rb
+++ app/controllers/form_validations/searches_controller.rb
@@ -4,15 +4,15 @@ class FormValidations::SearchesController < SearchesController
     @search.assign_attributes(search_params)
     @search.valid?
     respond_to do |format|
-      format.text { render partial: 'search/form', locals: { search: @search }, formats: [:html] } 
+      format.text { render partial: 'searches/form', locals: { search: @search }, formats: [:html] } 
     end
   end
   
-  def creates
+  def create
     @search = Search.new(search_params)
     @search.validate
     respond_to do |format|
-      format.text { render partial: 'search/form', locals: { search: @search }, formats: [:html] } 
+      format.text { render partial: 'searches/form', locals: { search: @search }, formats: [:html] } 
     end
   end
 end
