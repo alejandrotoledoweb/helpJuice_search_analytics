4923355bda8bb5ecf5c15f6ae13f79157e185701
alejandro toledo <atoledofr@gmail.com>
2021-09-23 21:54:07 -0500

    modify form_validations_controller and add debounce with yarn

--- app/javascript/controllers/form_validation_controller.js
+++ app/javascript/controllers/form_validation_controller.js
@@ -1,9 +1,14 @@
 import Rails from "@rails/ujs"
 import { Controller } from "stimulus"
+const debounce = require('lodash.debounce');
 
 export default class extends Controller {
   static targets  = [ "form", "output"]
   static values   = { url: String }
+
+  initialize() {
+    this.handleChange = debounce(this.handleChange, 500).bind(this)
+  }
 
   handleChange(event) {
     Rails.ajax({
